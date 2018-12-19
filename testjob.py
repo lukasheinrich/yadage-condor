fb7577da4253d959005fa3ec17e5c49d33857e7b
Lukas Heinrich <lukas.heinrich@gmail.com>
2018-12-19 16:43:48 +0100

    simple JSON API for condor based container execution

--- testjob.py
+++ testjob.py
@@ -1,11 +1,27 @@
+import os
+import sys
+import json
 from condor_json_api import CondorBackend
 
 backend = CondorBackend()
-spec = {
-   'image': 'lukasheinrich/busyboxwithafsdir',
-   'argv': ['sh','-c','echo hello needit > /afs/cern.ch/work/l/lheinric/sing/workdir/callbackdir.txt'],
-   'workdir': '/afs/cern.ch/work/l/lheinric/sing/workdir'
-}
 
-proxy = backend.submit(spec)
-print(json.dumps(proxy))
+def make_workdir_and_test_spec(workdir,message = 'Hello World'):
+   if os.path.exists(workdir):
+      raise RuntimeError('exists already choose a different one')
+   os.makedirs(workdir)
+   spec = {
+      'image': 'lukasheinrich/busyboxwithafsdir',
+      'argv': ['sh','-c','echo {message} > {workdir}/callbackdir.txt'.format(
+         message = message,
+         workdir = workdir
+      )],
+      'workdir': '/afs/cern.ch/work/l/lheinric/sing/workdir'
+   }
+   return spec
+
+   proxy = backend.submit(spec)
+   return proxy
+
+if __name__ == '__main__':
+   proxy = make_workdir_and_test_spec(sys.argv[1])
+   print(json.dumps(proxy))
